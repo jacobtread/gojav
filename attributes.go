5296bcbec0b4116d5275586966a4e07df2396c03
Jacobtread <jacobtread@gmail.com>
2022-03-17 19:24:15 +1300

    Inner Classes attribute

--- attributes.go
+++ attributes.go
@@ -7,7 +7,7 @@ func (r Reader) ReadAttributes(pool *ConstantPool) (*Attributes, error) {
 	}
 	out := &Attributes{
 		Size:       count,
-		Attributes: make([]*interface{}, count),
+		Attributes: make([]any, count),
 	}
 
 	var nameIndex uint16
@@ -27,6 +27,8 @@ func (r Reader) ReadAttributes(pool *ConstantPool) (*Attributes, error) {
 		switch name {
 		case "Code":
 			value, err = r.ReadCodeAttribute(pool)
+		case "InnerClasses":
+			value, err = r.ReadInnerClassesAttribute(pool)
 		default:
 			r.Discard(int64(length))
 		}
@@ -68,5 +70,72 @@ func (r Reader) ReadCodeAttribute(pool *ConstantPool) (*CodeAttribute, error) {
 	if err != nil {
 		return nil, err
 	}
+	return &out, nil
+}
+
+type InnerClassesAttribute struct {
+	Size    uint16
+	Entries []InnerClassesAttributeEntry
+}
+
+type InnerClassesAttributeEntry struct {
+	InnerNameIndex  uint16
+	OuterNameIndex  uint16
+	SimpleNameIndex uint16
+	AccessFlags     uint16
+
+	InnerName  string
+	OuterName  string
+	SimpleName string
+}
+
+func (r Reader) ReadInnerClassesAttribute(pool *ConstantPool) (*InnerClassesAttribute, error) {
+	count, err := r.ReadU2()
+	if err != nil {
+		return nil, err
+	}
+	out := InnerClassesAttribute{
+		Size:    count,
+		Entries: make([]InnerClassesAttributeEntry, count),
+	}
+
+	for i := uint16(0); i < count; i++ {
+		entry := InnerClassesAttributeEntry{}
+		err = r.ReadU2To(&entry.InnerNameIndex)
+		if err != nil {
+			return nil, err
+		}
+		err = r.ReadU2To(&entry.OuterNameIndex)
+		if err != nil {
+			return nil, err
+		}
+		err = r.ReadU2To(&entry.SimpleNameIndex)
+		if err != nil {
+			return nil, err
+		}
+		err = r.ReadU2To(&entry.AccessFlags)
+		if err != nil {
+			return nil, err
+		}
+
+		entry.SimpleName, err = pool.GetStringEntry(entry.InnerNameIndex)
+		if err != nil {
+			return nil, err
+		}
+		if entry.OuterNameIndex != 0 {
+			entry.OuterName, err = pool.GetStringEntry(entry.OuterNameIndex)
+			if err != nil {
+				return nil, err
+			}
+		}
+		if entry.SimpleNameIndex != 0 {
+			entry.SimpleName, err = pool.GetStringEntry(entry.SimpleNameIndex)
+			if err != nil {
+				return nil, err
+			}
+		}
+		out.Entries[i] = entry
+	}
+
 	return &out, nil
 }
